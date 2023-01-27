f6143f297a8404214db4148a5d4e1ac1fc62d836
Maria Dayekh <mariaaldayekh@gmail.com>
2023-01-27 15:57:21 +0200

    completed assignments

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,5 @@
 // Code EyesOnMe Component Here
+function EyesOnMe (){
+    <button onFocus={event => console.log("Good!")} onBlur={event => console.log("Hey! Eyes on me!")}>
+        Eyes on me</button>
+}
